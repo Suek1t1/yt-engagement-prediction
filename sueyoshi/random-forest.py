8ed925f3f9e09b376e1d72cf7464a5d27bb1b297
すえきち <mojiryouta@gmail.com>
2026-06-30 08:47:35 +0900

    a

--- sueyoshi/random-forest.py
+++ sueyoshi/random-forest.py
@@ -21,6 +21,7 @@ model = RandomForestRegressor(n_estimators=100, random_state=42)
 X = df[['views', 'dislikes', 'comment_count']]
 y = df['likes']
 
+
 # データをトレーニングセットとテストセットに分割する
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
